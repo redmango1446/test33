0b4698e4826dbd3e600b6de61386d011df54e14d
JI <JI@DESKTOP-IPKC005>
2022-03-10 18:07:33 +0900

    ccccc

--- src/main/java/test33/dao/jeonginTest.java
+++ src/main/java/test33/dao/jeonginTest.java
@@ -3,5 +3,10 @@ package test33.dao;
 public class jeonginTest {
 	public static void main(String[] args) {
 		System.out.println("YES");
+		System.out.println("YES");
+		System.out.println("YES");
+		System.out.println("YES");
+		System.out.println("YES");
+		System.out.println("YES");
 	}
 }
